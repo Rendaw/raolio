a12e8882e2aacbaa53a608dd200ff7af1b880f4a
Rendaw <rendaw@zarbosoft.com>
2013-10-22 11:16:57 +0900

    Allow disabling components, renamed client to gui for multiple future clients.

--- app/Tupfile.lua
+++ app/Tupfile.lua
@@ -3,11 +3,11 @@ local LinkFlags = ' -pthread -lboost_system -lboost_filesystem -lboost_regex -le
 local SharedObjects = Define.Objects
 {
 	Sources = Item()
-		:Include 'shared.cxx'
-		:Include 'core.cxx'
-		:Include 'hash.cxx'
-		:Include 'md5.c'
-		:Include 'network.cxx'
+		+ 'shared.cxx'
+		+ 'core.cxx'
+		+ 'hash.cxx'
+		+ 'md5.c'
+		+ 'network.cxx'
 }
 
 local icmbcMocOutputs = Define.Raw
@@ -17,41 +17,52 @@ local icmbcMocOutputs = Define.Raw
 	Command = 'moc qtaux.h > moc_qtaux.cxx'
 }
 
-raolioclient = Define.Executable
-{
-	Name = 'raolioclient',
-	Sources = Item()
-		:Include 'client.cxx'
-		:Include 'clientcore.cxx'
-		:Include(icmbcMocOutputs),
-	Objects = SharedObjects,
-	LinkFlags = LinkFlags .. ' -lvlc'
-}
+PackageExecutables = Item()
+PackageResources = Item()
 
-raolioserver = Define.Executable
-{
-	Name = 'raolioserver',
-	Sources = Item()
-		:Include 'server.cxx',
-	Objects = SharedObjects,
-	LinkFlags = LinkFlags
-}
-
-raolioremote = Define.Executable
-{
-	Name = 'raolioremote',
-	Sources = Item()
-		:Include 'remote.cxx',
-	Objects = SharedObjects,
-	LinkFlags = LinkFlags
-}
-
-if not IsDebug()
+if tup.getconfig 'BUILDGUI' ~= 'false'
 then
+	raoliogui = Define.Executable
+	{
+		Name = 'raoliogui',
+		Sources = Item() + 'gui.cxx' + 'clientcore.cxx' + icmbcMocOutputs,
+		Objects = SharedObjects,
+		LinkFlags = LinkFlags .. ' -lvlc'
+	}
+	PackageExecutables = PackageExecutables + raoliogui
+	PackageResources = PackageResources + '*.png'
+end
+
+if tup.getconfig 'BUILDSERVER'
+then
+	raolioserver = Define.Executable
+	{
+		Name = 'raolioserver',
+		Sources = Item() + 'server.cxx',
+		Objects = SharedObjects,
+		LinkFlags = LinkFlags
+	}
+
+	raolioremote = Define.Executable
+	{
+		Name = 'raolioremote',
+		Sources = Item() + 'remote.cxx',
+		Objects = SharedObjects,
+		LinkFlags = LinkFlags
+	}
+
+	PackageExecutables = PackageExecutables
+		+ (raolioserver)
+		+ (raolioremote)
+end
+
+if tup.getconfig 'PACKAGE' ~= 'false'
+then
+	if IsDebug() then error 'Debug builds aren\'t suitable for packaging.  Disable packaging or mark this build as release.' end
 	Package = Define.Package
 	{
-		Executables = raolioclient:Include(raolioremote):Include(raolioserver),
-		Resources = Item() + '*.png',
+		Executables = PackageExecutables,
+		Resources = PackageResources,
 		Licenses = Item() + '../license.txt'
 	}
 end
