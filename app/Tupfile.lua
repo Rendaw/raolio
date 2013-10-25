8570c262a8783880fa66abe5e9f8b30734a4d814
Rendaw <rendaw@zarbosoft.com>
2013-10-25 15:46:09 +0900

    Filled in and fixed some package dependencies.

--- app/Tupfile.lua
+++ app/Tupfile.lua
@@ -18,7 +18,7 @@ local SharedClientObjects = Define.Objects
 
 local PackageDependencies =
 	(tup.getconfig 'PLATFORM' == 'arch64' and "'boost>=1.54.0-3', 'boost-libs>=1.54.0-3', 'libev>=4.15-1'" or '') ..
-	(tup.getconfig 'PLATFORM' == 'ubuntu' and 'libboost-all-dev (>= 1.53.0-6), libev4 (>= 1.4.11-1)' or '')
+	(tup.getconfig 'PLATFORM' == 'ubuntu' and 'libboost-all-dev (>= 1.53.0-0), libev4 (>= 1.4.11-1)' or '')
 
 if tup.getconfig 'BUILDGUI' ~= 'false'
 then
@@ -40,7 +40,7 @@ then
 
 	local PackageDependencies = PackageDependencies ..
 		(tup.getconfig 'PLATFORM' == 'arch64' and ", 'qt5-base>=5.1.1-1', 'vlc>=2.1.0-3'" or '') ..
-		(tup.getconfig 'PLATFORM' == 'ubuntu' and ', libqt5core5 (>= 5.0.2), libqt5widgets5 (>= 5.0.2), libqt5gui5 (>= 5.0.2), libvlc5 (>= 2.0.8-1)' or '')
+		(tup.getconfig 'PLATFORM' == 'ubuntu' and ', libqt5core5 (>= 5.0.2), libqt5widgets5 (>= 5.0.2), libqt5gui5 (>= 5.0.2), libvlc5 (>= 2.0.8-0)' or '')
 	Package = Define.Package
 	{
 		Name = 'raoliogui',
@@ -93,8 +93,8 @@ then
 	}
 
 	local PackageDependencies = PackageDependencies ..
-		(tup.getconfig 'PLATFORM' == 'arch64' and ", 'readline>=6.2.004-1'" or '') ..
-		(tup.getconfig 'PLATFORM' == 'ubuntu' and ', readline6 (>= 6.2-9)' or '')
+		(tup.getconfig 'PLATFORM' == 'arch64' and ", 'readline>=6.2.004-1', 'vlc>=2.1.0-3'" or '') ..
+		(tup.getconfig 'PLATFORM' == 'ubuntu' and ', readline6 (>= 6.2-9), libvlc5 (>= 2.0.8-0)' or '')
 	Package = Define.Package
 	{
 		Name = 'raoliocli',
